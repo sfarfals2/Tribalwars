e813ac5c3b1c75088a0049739f7d06e47fcd9133
Fábio Carvalho <fabio_carvalho2007@hotmail.com>
2023-12-21 22:38:42 +0000

    Create attack.js

---
+++ attack.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name     3 - Enviar Ataques (Auto Planeador - Anti Ban)
+// @author   canixe
+// @fixedBy   xd
+// @version  4
+// @match    ://*.tribalwars.com.pt/*&screen=place*&try=confirm
+// @grant    none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $.getScript('https://dl.dropboxusercontent.com/s/ri9zym4a11op5jc/agendadorNoBanObfsSemLoader.js?dl=0'); void(0);
+})();
