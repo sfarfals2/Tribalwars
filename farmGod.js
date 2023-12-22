e58834043c9c2a23c830fddfe45af0f2264a085a
Fábio Carvalho <fabio_carvalho2007@hotmail.com>
2023-12-22 12:22:39 +0000

    Update farmGod.js
    improve performance

--- farmGod.js
+++ farmGod.js
@@ -3,7 +3,7 @@
 // @author Reformed
 // @email ...
 // @namespace ...
-// @version 1.1
+// @version 1.2
 // @grant Publico
 // @description AutoFarm para Tribal Wars, com alerta sonoro para captcha.
 // @match    ://*.tribalwars.com.pt/*&screen=am_farm*
